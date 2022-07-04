b6f63dfb841559555fe5e2b6403a5912b20a0de3
matt <matt@multicode.co.uk>
2022-07-04 15:04:17 +0000

    song list updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import ExampleComponent,{AnotherComponent} from './components/ExampleComponent';
 import Greeting from './components/Greeting';
+import SongList from './components/SongList';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <ExampleComponent />
       <AnotherComponent />
       <Greeting name="Matt" age="23" />
+      <Greeting name="Sally" age="43" />
+      <p className="firstPara" >This is another paragraph</p>
+      <SongList />
     </div>
   );
 }
